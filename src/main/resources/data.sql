a3fd5b95e52cb6255ef568f4afb0778904d57723
krystiandomian <krdomian@gmail.com>
2020-08-14 14:19:38 +0200

    - create project from start.spring.io - add H2 database with simply schema.sql and data.sql files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into Users values ( 1,'Krystian', 'Domian', 'krdomian@gmail.com', 'Katowice' );
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users (
+                         id int,
+                         LastName varchar(255),
+                         FirstName varchar(255),
+                         Email varchar(255),
+                         City varchar(255)
+);
